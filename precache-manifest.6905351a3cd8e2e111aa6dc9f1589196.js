70b80a1f025a199a10bb9887ce21b270dc9cd637
Zachary Richmond <zacharyrichmond@zacharys-mbp.attlocal.net>
2019-04-20 16:09:48 -0400

    Updates

--- precache-manifest.6905351a3cd8e2e111aa6dc9f1589196.js
+++ precache-manifest.6905351a3cd8e2e111aa6dc9f1589196.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/cse6242-project/static/js/runtime~main.02671fbc.js"
   },
   {
-    "revision": "134bdd603b1524e3af32",
-    "url": "/cse6242-project/static/js/main.6006c0c5.chunk.js"
+    "revision": "329957cdc9996a2ad0dc",
+    "url": "/cse6242-project/static/js/main.d537aada.chunk.js"
   },
   {
     "revision": "4bae51fa6e8c0b3cda0f",
     "url": "/cse6242-project/static/js/2.71cb5546.chunk.js"
   },
   {
-    "revision": "134bdd603b1524e3af32",
+    "revision": "329957cdc9996a2ad0dc",
     "url": "/cse6242-project/static/css/main.d781d84b.chunk.css"
   },
   {
-    "revision": "d16aebb8356cc88f3b2f2a531501cfa7",
+    "revision": "a4607e91c2adef5e4876cf5953956e0b",
     "url": "/cse6242-project/index.html"
   }
 ];
